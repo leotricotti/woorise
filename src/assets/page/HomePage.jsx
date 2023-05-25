706a1b62b03ad1f01e678fe99017ebb44533a9d9
Leonardo Tricotti <tricottileo@gmail.com>
2023-05-25 10:51:05 -0300

    commit

---
+++ src/assets/page/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Heder";
+import styles from "./assets/page/homePage.module.css";
+
+function HomePage() {
+  return (
+    <main className={styles.siteContainer}>
+      <Header />
+    </main>
+  );
+}
+
+export default HomePage;
--- src/components/header/Heder.js
+++
@@ -1,2 +0,0 @@
-import {Link} 
-import styles from "./header.module.css";
