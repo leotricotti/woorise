9b88e13274cc98b96a2947d34eab12c6cfbd5b4e
Leonardo Tricotti <tricottileo@gmail.com>
2023-05-28 14:27:08 -0300

    commit

--- src/assets/data/togleMenuData.js
+++ src/assets/data/togleMenuData.js
@@ -2,7 +2,8 @@ const menuToggleData = [
   { id: 1, title: "Features", link: "/" },
   { id: 2, title: "Templates ", link: "/" },
   { id: 3, title: "Pricing", link: "/" },
-  { id: 4, title: "Start Free ", link: "/" },
+  { id: 4, title: "Login ", link: "/" },
+  { id: 5, title: "Start Free", link: "/" },
 ];
 
 export default menuToggleData;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/woorise-logo.svg";
 import menuToggleData from "../../assets/data/togleMenuData";
@@ -5,9 +6,12 @@ import styles from "./header.module.css";
 
 function Header() {
   const data = menuToggleData;
+  const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <header className={styles.siteHeader}>
+    <header
+      className={`${styles.siteHeader} ${navOpen ? styles.headerOpen : ""}`}
+    >
       <div className={styles.wrap}>
         <div className={styles.titleArea}>
           <Link to="/">
@@ -15,26 +19,27 @@ function Header() {
           </Link>
         </div>
         <button
-          className={styles.menuToogle}
+          className={styles.menuToggle}
           aria-expanded={false}
           aria-pressed={false}
           role="button"
-          id="mobile-genesis-nav-primary"
-        ></button>
-        <nav
-          className={styles.navPrimary}
-          aria-label="Main navigation"
-          id="genesis-nav-primary"
+          onClick={() => setNavOpen(!navOpen)}
         >
-          <ul id="menu-main" className={styles.navMenu}>
-            {data.map((item) => (
-              <li key={item.id} id="menu-item-73" className={styles.menuItem}>
-                <Link to="#">{item.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+          <span className={styles.toggleLine}></span>
+        </button>
       </div>
+      <nav className={styles.navPrimary} aria-label="Main navigation">
+        <ul
+          id="menu-main"
+          className={`${styles.navMenu} ${navOpen ? styles.navOpen : ""}`}
+        >
+          {data.map((item) => (
+            <li key={item.id} id="menu-item-73" className={styles.menuItem}>
+              <Link to="#">{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
