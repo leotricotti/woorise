b85e4cc26ce67294c01e569bf460b7bfdc684680
Leonardo Tricotti <tricottileo@gmail.com>
2023-05-28 17:30:08 -0300

    herosection starter files

--- src/assets/page/HomePage.jsx
+++ src/assets/page/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/header/Header";
+import HeroSection from "../../components/heroSection/HeroSection";
 import styles from "./homePage.module.css";
 
 function HomePage() {
   return (
     <main className={styles.siteContainer}>
       <Header />
+      <HeroSection />
     </main>
   );
 }
---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+import styles from "./heroSection.module.css";
+
+import React from "react";
+
+function HeroSection() {
+  return <div>HeroSection</div>;
+}
+
+export default HeroSection;
