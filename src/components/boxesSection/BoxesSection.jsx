175616e4b109525bec2b5efa518ec59de515c913
Leonardo Tricotti <tricottileo@gmail.com>
2023-05-30 20:37:24 -0300

    commit

--- src/components/boxesSection/BoxesSection.jsx
+++ src/components/boxesSection/BoxesSection.jsx
@@ -12,17 +12,16 @@ function BoxesSection() {
 
   return (
     <section className={styles.sectionBoxes}>
-      <div className={styles.heading} key={data.heading.id}>
-        <h2 className={styles.headingTitle}>{data.heading.title}</h2>
-        <p className={styles.headingParagraph}>{data.heading.text}</p>
+      <div className={styles.headingContainer} key={data.heading.id}>
+        <h2 className={styles.title}>{data.heading.title}</h2>
+        <p className={styles.text}>{data.heading.text}</p>
       </div>
-
       <div className={styles.flexContainer}>
         {data.cards.map((item) => (
           <div className={styles.boxes} key={item.id}>
             <img src={item.url} alt={item.alt} />
-            <h3 className={styles.title}>{item.title}</h3>
-            <p className={styles.paragraph}>{item.text}</p>
+            <h3 className={styles.boxTitle}>{item.title}</h3>
+            <p className={styles.boxText}>{item.text}</p>
             <Link to={"/"} target="_blank" className={styles.navLink}>
               {item.link}
             </Link>
